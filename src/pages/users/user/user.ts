241bf70ee0dcbf5f1c004ab04b8bd348a4f32704
Paulina  Sudjatmiko <paulina@Paulinas-MacBook-Air.local>
2017-07-04 16:44:02 +1000

    popping pages

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
-import { NavParams } from "ionic-angular";
+import { NavParams, NavController } from "ionic-angular";
 
 @Component({
   selector: 'page-user',
@@ -10,9 +10,17 @@ import { NavParams } from "ionic-angular";
 export class UserPage implements OnInit {
   name: string;
 
-  constructor (private navParams: NavParams) {}
+  constructor (
+    private navParams: NavParams,
+    private navCtrl: NavController) {
+
+    }
 
   ngOnInit() {
     this.name = this.navParams.get('userName');
+  }
+  onGoBack() {
+    // this.navCtrl.pop(); //Navigate to the previous page
+    this.navCtrl.popToRoot(); //Navigate back to the root
   }
 }
